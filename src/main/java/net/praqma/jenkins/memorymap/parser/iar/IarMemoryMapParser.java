ff4328355dc09e83449e5e0f7346144291804844
Kay van der Zander <kz@ebmpapst-hs.nl>
2018-05-18 08:57:01 +0200

    add all memory regexs
    add EWARM, STM8, AVR  memory regex

--- src/main/java/net/praqma/jenkins/memorymap/parser/iar/IarMemoryMapParser.java
+++ src/main/java/net/praqma/jenkins/memorymap/parser/iar/IarMemoryMapParser.java
@@ -35,7 +35,6 @@ import net.praqma.jenkins.memorymap.util.MemoryMapMemorySelectionError;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -46,8 +45,8 @@ import org.jenkinsci.Symbol;
 
 /**
  *
- * @author ebmpapst-hs <Kay van der Zander>
- * IAR ELF Linker V6.40.2.53884 for ARM
+ * @author Kay van der Zander
+ * IAR map files used from ARM, STM8 and AVR.
  */
 public class IARMemoryMapParser extends AbstractMemoryMapParser {
 
@@ -55,18 +54,38 @@ public class IARMemoryMapParser extends AbstractMemoryMapParser {
     /*
      * Flash
      */
-    private static final Pattern TEXT_DOT = Pattern.compile("\\.text\\s+\\S+\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern CONST_DOT = Pattern.compile("\\.rodata\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern NOINIT = Pattern.compile("\\.noinit\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern EWARM_TEXT = Pattern.compile("\\.text\\s+\\S+\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern STM8_TEXT = Pattern.compile("\\.near_func\\.text\\s+ro\\scode\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern AVR_CODE = Pattern.compile("CODE\\s\\w+\\s\\-\\s\\w+\\s\\((\\S+)\\sbytes\\)", Pattern.MULTILINE);
+    private static final Pattern EWARM_CONST = Pattern.compile("\\.rodata\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern EWARM_NOINIT = Pattern.compile("\\.noinit\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern STM8_NOINIT = Pattern.compile("\.near\.noinit\s+uninit\s+\S+\s+(\S+)", Pattern.MULTILINE);
+    // the onces here below are the same over all IAR compiler version (EWARM, STM8 and AVR).
+    private static final Pattern IAR_INIT_TABLE = Pattern.compile("\\.iar\\.init_table\\s+const\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern IAR_INTERRUPT_VECTOR_TABLE = Pattern.compile("\\.intvec\\s+(const|ro\\scode)\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern IAR_INIT_BYTES = Pattern.compile("Initializer\\sbytes\\s+ro\\sdata\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern STM8_IAR_VREGS = Pattern.compile("\\.vregs\\s+uninit\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern IAR_CHECKSUM = Pattern.compile("\.checksum\s+const\s+\S+\s+(\S+)", Pattern.MULTILINE);
     /*
      * Ram
      */
-    private static final Pattern BSS_DOT = Pattern.compile("\\.bss\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern DATA = Pattern.compile("\\.data\\s+inited\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern EWARM_BSS = Pattern.compile("\\.bss\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern STM8_BSS_TINY = Pattern.compile("\\.tiny\\.bss\\s+zero\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern STM8_BSS_NEAR = Pattern.compile("\\.near\\.bss\\s+zero\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern EWARM_DATA = Pattern.compile("\\.data\\s+inited\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern STM8_DATA = Pattern.compile("\\.tiny\\.data\\s+inited\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    private static final Pattern AVR_DATA = Pattern.compile("\\sDATA\\s\\w+\\s\\-\\s\\w+\\s\\((\\S+)\\sbytes\\)", Pattern.MULTILINE);
+    // the onces here below are the same over IAR compiler version (EWARM, STM8. AVR is different see normal AVR_CODE).
+    private static final Pattern IAR_CSTACK = Pattern.compile("CSTACK\\s+uninit\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
+    /*
+     * EEPROM
+     */
+    private static final Pattern AVR_EEPROM = Pattern.compile("XDATA\\s\\w+\\s\\-\\s\\w+\\s\\((\\S+)\\sbytes\\)", Pattern.MULTILINE);
 
     @DataBoundConstructor
     public IARMemoryMapParser(String parserUniqueName, String mapFile, String configurationFile, Integer wordSize, List<MemoryMapGraphConfiguration> graphConfiguration, Boolean bytesOnGraph) {
-        super(parserUniqueName, mapFile, configurationFile, wordSize, bytesOnGraph, graphConfiguration, TEXT_DOT, CONST_DOT, NOINIT, BSS_DOT, DATA);
+        super(parserUniqueName, mapFile, configurationFile, wordSize, bytesOnGraph, graphConfiguration,
+        EWARM_TEXT, EWARM_CONST, EWARM_NOINIT, EWARM_BSS, EWARM_DATA,);
     }
 
     public IARMemoryMapParser() {
