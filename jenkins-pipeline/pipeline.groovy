24f0bfb377c28b9b393d59f71a69e409b27853f5
Mads Nielsen <man@praqma.net>
2018-07-12 12:05:42 +0200

    Fixed Jenkinsfile

--- jenkins-pipeline/pipeline.groovy
+++ jenkins-pipeline/pipeline.groovy
@@ -1,4 +1,9 @@
 multibranchPipelineJob("Memory Map Jenkins Plugin") {
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('jenkins-pipeline/Jenkinsfile')
+        }
+    }
     branchSources {
         git {
             credentialsId("github")
@@ -11,7 +16,8 @@ multibranchPipelineJob("Memory Map Jenkins Plugin") {
     }
 
     configure {
-        it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits' << 'jenkins.plugins.git.traits.CloneOptionTrait' {
+        def traitBlock = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+        traitBlock << 'jenkins.plugins.git.traits.CloneOptionTrait' {
             extension(class: 'hudson.plugins.git.extensions.impl.CloneOption') {
                 shallow(false)
                 noTag(false)
@@ -20,5 +26,8 @@ multibranchPipelineJob("Memory Map Jenkins Plugin") {
                 honorRefspec(false)
             }
         }
+
+        traitBlock << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' { }
     }
+
 }
