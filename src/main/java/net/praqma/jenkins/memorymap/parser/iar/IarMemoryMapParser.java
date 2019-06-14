e72dbf61711171fc79f960de51253ea22393ef20
Thierry Lacour <thi@praqma.net>
2019-06-14 15:35:25 +0200

    Move patterns to a map for simpler access

--- src/main/java/net/praqma/jenkins/memorymap/parser/iar/IarMemoryMapParser.java
+++ src/main/java/net/praqma/jenkins/memorymap/parser/iar/IarMemoryMapParser.java
@@ -26,8 +26,6 @@ package net.praqma.jenkins.memorymap.parser.iar;
 import hudson.Extension;
 import net.praqma.jenkins.memorymap.graph.MemoryMapGraphConfiguration;
 import net.praqma.jenkins.memorymap.parser.AbstractMemoryMapParser;
-import net.praqma.jenkins.memorymap.parser.MemoryMapConfigFileParserDelegate;
-import net.praqma.jenkins.memorymap.parser.MemoryMapMapParserDelegate;
 import net.praqma.jenkins.memorymap.parser.MemoryMapParserDescriptor;
 import net.praqma.jenkins.memorymap.result.MemoryMapConfigMemory;
 import net.praqma.jenkins.memorymap.result.MemoryMapConfigMemoryItem;
@@ -37,6 +35,7 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
@@ -50,42 +49,49 @@ import org.jenkinsci.Symbol;
  */
 public class IarMemoryMapParser extends AbstractMemoryMapParser {
 
+    private static final HashMap<String, Pattern> PATTERNS = new HashMap<String, Pattern>() {{
+        /*********
+         * FLASH
+         ********/
+        put(".text",       Pattern.compile("\\.text\\s+\\S+\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));          // EWARM_TEXT
+        put(".near_func",  Pattern.compile("\\.near_func\\.text\\s+ro\\scode\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE)); // STM8_TEXT
+        put("CODE",        Pattern.compile("CODE\\s\\w+\\s\\-\\s\\w+\\s\\((\\S+)\\sbytes\\)", Pattern.MULTILINE));    // AVR_CODE
+        put(".rodata",     Pattern.compile("\\.rodata\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));                // EWARM_CONST
+        put(".noinit",     Pattern.compile("\\.noinit\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));                // EWARM_NOINIT
+        put(".near",       Pattern.compile("\\.near\\.noinit\\s+uninit\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));       // STM8_NOINIT
+        put(".iar",        Pattern.compile("\\.iar\\.init_table\\s+const\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));     // IAR_INIT_TABLE
+        put(".intvec",     Pattern.compile("\\.intvec\\s+(?:const|ro\\scode)\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE)); // IAR_INTERRUPT_VECTOR_TABLE
+        put("Initializer", Pattern.compile("Initializer\\sbytes\\s+ro\\sdata\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE)); // IAR_INIT_BYTES
+        put(".vregs",      Pattern.compile("\\.vregs\\s+uninit\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));               // STM8_IAR_VREGS
+        put(".checksum",   Pattern.compile("\\.checksum\\s+const\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));             // IAR_CHECKSUM
 
-    /*
-     * Flash
-     */
-    private static final Pattern EWARM_TEXT = Pattern.compile("\\.text\\s+\\S+\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern STM8_TEXT = Pattern.compile("\\.near_func\\.text\\s+ro\\scode\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern AVR_CODE = Pattern.compile("CODE\\s\\w+\\s\\-\\s\\w+\\s\\((\\S+)\\sbytes\\)", Pattern.MULTILINE);
-    private static final Pattern EWARM_CONST = Pattern.compile("\\.rodata\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern EWARM_NOINIT = Pattern.compile("\\.noinit\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern STM8_NOINIT = Pattern.compile("\\.near\\.noinit\\s+uninit\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    // the onces here below are the same over all IAR compiler version (EWARM, STM8 and AVR).
-    private static final Pattern IAR_INIT_TABLE = Pattern.compile("\\.iar\\.init_table\\s+const\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern IAR_INTERRUPT_VECTOR_TABLE = Pattern.compile("\\.intvec\\s+(const|ro\\scode)\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern IAR_INIT_BYTES = Pattern.compile("Initializer\\sbytes\\s+ro\\sdata\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern STM8_IAR_VREGS = Pattern.compile("\\.vregs\\s+uninit\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern IAR_CHECKSUM = Pattern.compile("\\.checksum\\s+const\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    /*
-     * Ram
-     */
-    private static final Pattern EWARM_BSS = Pattern.compile("\\.bss\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern STM8_BSS_TINY = Pattern.compile("\\.tiny\\.bss\\s+zero\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern STM8_BSS_NEAR = Pattern.compile("\\.near\\.bss\\s+zero\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern EWARM_DATA = Pattern.compile("\\.data\\s+inited\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern STM8_DATA = Pattern.compile("\\.tiny\\.data\\s+inited\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    private static final Pattern AVR_DATA = Pattern.compile("\\sDATA\\s\\w+\\s\\-\\s\\w+\\s\\((\\S+)\\sbytes\\)", Pattern.MULTILINE);
-    // the onces here below are the same over IAR compiler version (EWARM, STM8. AVR is different see normal AVR_CODE).
-    private static final Pattern IAR_CSTACK = Pattern.compile("CSTACK\\s+uninit\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
-    /*
-     * EEPROM
-     */
-    private static final Pattern AVR_EEPROM = Pattern.compile("XDATA\\s\\w+\\s\\-\\s\\w+\\s\\((\\S+)\\sbytes\\)", Pattern.MULTILINE);
+        /*********
+         * RAM
+         ********/
+         put(".bss",   Pattern.compile("\\.bss\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));                     // EWARM_BSS
+         put(".tiny",  Pattern.compile("\\.tiny\\.bss\\s+zero\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));              // STM8_BSS_TINY
+         put(".near",  Pattern.compile("\\.near\\.bss\\s+zero\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));              // STM8_BSS_NEAR
+         put(".data",  Pattern.compile("\\.data\\s+inited\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));                  // EWARM_DATA
+         put(".tiny",  Pattern.compile("\\.tiny\\.data\\s+inited\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));           // STM8_DATA
+         put("sDATA",  Pattern.compile("\\sDATA\\s\\w+\\s\\-\\s\\w+\\s\\((\\S+)\\sbytes\\)", Pattern.MULTILINE));   // AVR_DATA
+         put("CSTACK", Pattern.compile("CSTACK\\s+uninit\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE));                   // IAR_CSTACK
+
+        /*********
+         * EEPROM
+         ********/
+        put("XDATA", Pattern.compile("XDATA\\s\\w+\\s\\-\\s\\w+\\s\\((\\S+)\\sbytes\\)", Pattern.MULTILINE));   // AVR_EEPROM
+    }};
 
     @DataBoundConstructor
     public IarMemoryMapParser(String parserUniqueName, String mapFile, String configurationFile, Integer wordSize, List<MemoryMapGraphConfiguration> graphConfiguration, Boolean bytesOnGraph) {
-        super(parserUniqueName, mapFile, configurationFile, wordSize, bytesOnGraph, graphConfiguration,
-        EWARM_TEXT, EWARM_CONST, EWARM_NOINIT, EWARM_BSS, EWARM_DATA);
+        super(
+            parserUniqueName,
+            mapFile,
+            configurationFile,
+            wordSize,
+            bytesOnGraph,
+            graphConfiguration
+        );
     }
 
     public IarMemoryMapParser() {
@@ -94,23 +100,32 @@ public class IarMemoryMapParser extends AbstractMemoryMapParser {
 
     @Override
     public MemoryMapConfigMemory parseConfigFile(File f) throws IOException {
-        MemoryMapConfigMemory config = new MemoryMapConfigMemory();
-        CharSequence sequence = createCharSequenceFromFile(f);
+        return new MemoryMapConfigMemory();
+    }
+
+    @Override
+    public MemoryMapConfigMemory parseMapFile(File f, MemoryMapConfigMemory config) throws IOException {
+        CharSequence text = createCharSequenceFromFile(f);
         for (MemoryMapGraphConfiguration graph : getGraphConfiguration()) {
-            String[] split = graph.getGraphDataList().split(",");
-            for (String s : split) {
-                String[] multiSections = s.trim().split("\\+");
-                for (String ms : multiSections) {
-                    Matcher m = MemoryMapConfigFileParserDelegate.getPatternForMemoryLayout(ms.replace(" ", "")).matcher(sequence);
+            String[] graphSections = graph.getGraphDataList().split(",");
+            for (String sectionNotation : graphSections) {
+                String[] sections = sectionNotation.trim().split("\\+");
+                for (String section : sections) {
+                    Matcher matcher = getMatchingPattern(section).matcher(text);
+
                     MemoryMapConfigMemoryItem item = null;
-                    while (m.find()) {
-                        item = new MemoryMapConfigMemoryItem(m.group(1), m.group(3), m.group(5));
+                    while (matcher.find()) {
+                        // Need to build a MemoryMapConfigMemoryItem with as much data as we can squeeze out of the map file:
+                        // MemoryMapConfigMemoryItem(String name, String origin, String length, String used, String unused)
+                        item = new MemoryMapConfigMemoryItem(section, "not_sure", matcher.group(1));
                         config.add(item);
                     }
 
                     if (item == null) {
-                        logger.logp(Level.WARNING, "parseConfigFile", AbstractMemoryMapParser.class.getName(), String.format("parseConfigFile(List<MemoryMapGraphConfiguration> graphConfig, File f) non existing item: %s", s));
-                        throw new MemoryMapMemorySelectionError(String.format("No match found for program memory named %s", s));
+                        logger.logp(
+                            Level.WARNING, "parseConfigFile", this.getClass().getName(), String.format("parseConfigFile(List<MemoryMapGraphConfiguration> graphConfig, File f) non existing item: %s", section)
+                        );
+                        throw new MemoryMapMemorySelectionError(String.format("No match found for section named %s", section));
                     }
                 }
 
@@ -119,24 +134,13 @@ public class IarMemoryMapParser extends AbstractMemoryMapParser {
         return config;
     }
 
-    @Override
-    public MemoryMapConfigMemory parseMapFile(File f, MemoryMapConfigMemory config) throws IOException {
-        CharSequence sequence = createCharSequenceFromFile(f);
-
-        for (MemoryMapConfigMemoryItem item : config) {
-            Matcher matcher = MemoryMapMapParserDelegate.getPatternForMemorySection(item.getName()).matcher(sequence);
-            boolean found = false;
-            while (matcher.find()) {
-                item.setUsed(matcher.group(8));
-                item.setUnused(matcher.group(10));
-                found = true;
-            }
-            if (!found) {
-                logger.logp(Level.WARNING, "parseMapFile", AbstractMemoryMapParser.class.getName(), String.format("parseMapFile(File f, MemoryMapConfigMemory configuration) non existing item: %s", item));
-                throw new MemoryMapMemorySelectionError(String.format("Linker command element %s not found in .map file", item));
-            }
+    private Pattern getMatchingPattern(String section) {
+        if (PATTERNS.containsKey(section)) {
+            return PATTERNS.get(section);
         }
-        return config;
+
+        logger.log(Level.WARNING, "No matching pattern defined for section: " + section + "\nFalling back to default pattern.");
+        return Pattern.compile(section + "\\s+\\S+\\s+\\S+\\s+\\S+\\s+(\\S+)", Pattern.MULTILINE);
     }
 
     @Override
